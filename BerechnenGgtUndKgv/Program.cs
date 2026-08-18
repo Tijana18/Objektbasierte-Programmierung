45159471852aa16e102c6d5f2e71a016d758ad91
Tijana Filipovic <tijanafilipovic@hotmail.com>
2026-08-18 12:31:13 +0200

    Initial commit

---
+++ BerechnenGgtUndKgv/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    // GGT berechnen
+    static int GGT(int a, int b)
+    {
+        while (b != 0)
+        {
+            int rest = a % b;
+            a = b;
+            b = rest;
+        }
+        return a;
+    }
+    // KGV berechnen
+    static int KGV(int a, int b)
+    {
+        return Math.Abs(a * b) / GGT(a, b);
+    }
+    static void Main()
+    {
+        Console.Write("Gib die erste Zahl ein: ");
+        int zahl1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Gib die zweite Zahl ein: ");
+        int zahl2 = Convert.ToInt32(Console.ReadLine());
+
+        int ggt = GGT(zahl1, zahl2);
+        int kgv = KGV(zahl1, zahl2);
+
+        Console.WriteLine("GGT = " + ggt);
+        Console.WriteLine("KGV = " + kgv);
+    }
+}
